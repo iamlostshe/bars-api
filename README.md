e25a81c5cff4fc9a51930e374cc5aedb4b38682c
ivan <vanamelcikov7275@gmail.com>
2025-03-12 23:57:33 +0400

    code: standartizate to pep-8

--- README.md
+++ README.md
@@ -1,20 +1,44 @@
-# BarsAPI
-A python library to get data from Bars School Diary
+# bars-api
 
-## Using
+Python библиотека для получения информации из Школьного днвника BARS.
 
-```py
-import barsapi
+## Быстрый старт
+
+```python
+from barsapi import BarsAPI
+
+# TODO: Доделать работу с HOST
+HOST = "YOUR_HOST_HERE"
+COOKIE = "YOUR_COOKIE_HERE"
+
+api = BarsAPI(HOST, COOKIE)
+
+print(api.get_class_year_info())
+print(api.get_person_data())
+print(api.get_school_info())
+print(api.get_summary_marks_url())
 ```
 
-```py
-getMarks(cookie)
-```
+## get_class_year_info
 
-```py
-getData(cookie)
-```
+Данные о классе ученика:
 
-```py
-getClass(cookie)
+```json
+{
+    "study_level": 10, // Класс в котором учится пользователь
+    "letter": "А", // Буква класса в котором учится пользователь
+    "form_master": "Иванов Иван Иванович", // ФИО классного руководителя
+    "form_master_photo": "noavatar_f_big.png", // Аватарка классного руководителя
+    "form_master_male": true, // Пол классного руководителя (true - мужской, false - женский)
+    "specialization": "нет", // ? Специализация
+    "photo": "noavatar_big.gif", // Аватарка пользователя
+    "pupils": [ // Ученики (однокласники пользователя)
+        {
+            "fullname": "Иванов Иван Иванович", // ФИО однокласника
+            "photo": "", // Аватарка однокласника
+            "male": true // Пол однокласника (true - мужской, false - женский)
+        },
+        ...
+    ]
+}
 ```
--- barsapi.py
+++ barsapi.py
@@ -1,72 +1,74 @@
+"""Base pars module."""  # noqa: CPY001
+
+import datetime as dt
+
 import requests
 from fake_useragent import UserAgent
 
-user = UserAgent().random
+TIMEOUT = 20
 
-login_url = "https://school.r-19.ru/auth/login"
-data_url = "https://school.r-19.ru/api/ProfileService/GetPersonData"
-school_url = "https://school.r-19.ru/api/SchoolService/getSchoolInfo"
-class_url = "https://school.r-19.ru/api/SchoolService/getClassYearInfo"
-diary_url = "https://school.r-19.ru/api/MarkService/GetSummaryMarks"
+USER_AGENT = UserAgent().random
 
-def getMarks(cookie: str):
-    headers = {
-        "User-Agent": user,
-        "Cookie": f"sessionid={cookie}"
+HOST = "YOUR_HOST_HERE"
+COOKIE = "YOUR_COOKIE_HERE"
+
+HEADERS = {
+    "user-Agent": USER_AGENT,
+    "Cookie": COOKIE,
+}
+
+GET_CLASS_YEAR_INFO_URL = f"https://{HOST}/api/SchoolService/getClassYearInfo"
+GET_PERSON_DATA_URL = f"https://{HOST}/api/ProfileService/GetPersonData"
+GET_SCHOOL_INFO_URL = f"https://{HOST}/api/SchoolService/getSchoolInfo"
+GET_SUMMARY_MARKS_URL = f"https://{HOST}/api/MarkService/GetSummaryMarks"
+
+
+def get_class_year_info() -> dict:
+    """Данные о классе ученика.
+
+    Returns:
+        Данные о классе ученика.
+
+    """  # noqa: RUF002
+    r = requests.post(GET_CLASS_YEAR_INFO_URL, headers=HEADERS, timeout=TIMEOUT)
+    return r.json()
+
+
+def get_person_data() -> dict:
+    """Данные об ученике.
+
+    Returns:
+        Данные об ученике.
+
+    """  # noqa: RUF002
+    r = requests.post(GET_PERSON_DATA_URL, headers=HEADERS, timeout=TIMEOUT)
+    return r.json()
+
+
+def get_school_info() -> dict:
+    """Данные о школе пользователя.
+
+    Returns:
+        Данные о школе пользователя.
+
+    """  # noqa: RUF002
+    r = requests.post(GET_SCHOOL_INFO_URL, headers=HEADERS, timeout=TIMEOUT)
+    return r.json()
+
+
+def get_summary_marks_url() -> dict:
+    """Данные об оценках пользователя.
+
+    Returns:
+        Данные об оценках пользователя.
+
+    """  # noqa: RUF002
+    params = {
+        "date": dt.datetime.now().strftime("%Y-%m-%d"),  # noqa: DTZ005
     }
 
-    response = requests.post(data_url, headers=headers)
-    resp = response.json()
-    data = ""
-    for i in resp["indicators"]:
-        data += f"{i['name']} | {i['value']}\n"
-
-    return data
-
-def getData(cookie: str):
-    headers = {
-        "User-Agent": user,
-        "Cookie": f"sessionid={cookie}"
-    }
-
-    response = requests.post(data_url, headers=headers)
-    resp = response.json()
-    data = ""
-    try:
-        return f"{resp['user_fullname']}\nШкола: {resp['selected_pupil_school']}"
-    except Exception:
-        return None
-    
-def getClass(cookie: str):
-    headers = {
-        "User-Agent": user,
-        "Cookie": f"sessionid={cookie}"
-    }
-
-    response = requests.post(class_url, headers=headers)
-    resp = response.json()
-    data = ""
-    try:
-        for i in resp["pupils"]:
-            data += f"{i['fullname']}\n"
-        return f"Классный руководитель: {resp['form_master']}\nКласс: \n{data}"
-    except Exception:
-        return None    
-    
-def getDiary(cookie: str):
-    headers = {
-        "User-Agent": user,
-        "Cookie": f"sessionid={cookie}"
-    }
-
-    #diary_url = f"https://school.r-19.ru/api/MarkService/GetSummaryMarks?date={str(datetime.datetime.now())}"
-
-    '''response = requests.get(diary_url, headers=headers)
-    resp = response.json()
-    data = ""
-    try:
-        for i in resp["discipline_marks"]:
-            data += f"{i['discipline']} | {i['mark']}"
-        return f"{data}"
-    except Exception:
-        return None'''
+    r = requests.get(
+        url=GET_SUMMARY_MARKS_URL, headers=HEADERS,
+        params=params, timeout=TIMEOUT,
+    )
+    return r.json()
